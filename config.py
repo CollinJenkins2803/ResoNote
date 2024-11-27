e3d35c5d40c1959bf08597a5388de3cc144e74e9
Collin Jenkins <cpj00003@mix.wvu.edu>
2024-11-27 12:17:45 -0500

    ResoNote/uploads

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
-    UPLOAD_FOLDER = '/uploads'
+    UPLOAD_FOLDER = 'ResoNote/uploads/'
     BUFFER_SIZE_LIMIT = 300 * 1024 * 1024  # 300 MB
     CHUNK_LENGTH_MS = 600000  # 10 minutes
     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
